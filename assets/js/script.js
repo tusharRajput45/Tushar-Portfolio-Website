1df1ddd79d1ee7890c6a7519ca27f01960d83602
tusharRajput45 <111507866+tusharRajput45@users.noreply.github.com>
2025-06-09 21:45:53 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,7 +58,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
+            document.title = "Portfolio | Tushar Rajput";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
