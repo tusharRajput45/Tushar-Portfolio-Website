553ffaa9c5e6751893d8ebf917c542fcdd1d6026
X-tusharRajput <tushar.rajput.dmp@gmail.com>
2025-06-17 10:57:58 +0530

    Tushar Rajput

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,7 +70,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["React Js development", "Node Js Backend development", "Web designing", "MERN development", "web development","UI/UX Developer"],
+    strings: ["React JS Development", "Node JS Backend Development", "Web Designer", "MERN Stack Development", "Web Development","UI/UX Developer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
